b187ed6ece0b5495ea8ad9c50f5bcc0f21185a39
subhrajit gupta <subhgupta@deloitte.com>
2025-01-03 15:15:44 +0530

    change in backend jenkins

--- cpgram-backend/Jenkinsfile.groovy
+++ cpgram-backend/Jenkinsfile.groovy
@@ -1,31 +1,58 @@
 pipeline {
     agent any
+
     environment {
-        DOCKER_CREDENTIALS_ID = 'dockerhub-credentials-id'
-        DOCKER_IMAGE = 'cpgram/backend-application'
+        DOCKER_IMAGE = "cpgram/backend-application:latest"
+        DOCKER_HUB_CREDENTIALS = 'dockerhub-creds' // This matches your Jenkins credentials ID
     }
+
     stages {
         stage('Checkout Code') {
             steps {
+                echo 'Checking out code...'
                 checkout scm
             }
         }
+
         stage('Build Docker Image') {
             steps {
-                sh 'docker build -t $DOCKER_IMAGE:latest .'
+                echo 'Building Docker image...'
+                sh 'docker build -t $DOCKER_IMAGE .'
             }
         }
+
         stage('Push Docker Image') {
             steps {
-                withDockerRegistry([credentialsId: "$DOCKER_CREDENTIALS_ID", url: ""]) {
-                    sh 'docker push $DOCKER_IMAGE:latest'
+                echo 'Pushing Docker image to Docker Hub...'
+                withDockerRegistry(credentialsId: "$DOCKER_HUB_CREDENTIALS", url: '') {
+                    sh 'docker push $DOCKER_IMAGE'
                 }
             }
         }
+
+        stage('Deploy Docker Image') {
+            steps {
+                echo 'Deploying Docker image...'
+                sh '''
+                docker pull $DOCKER_IMAGE
+                docker stop backend-container || true
+                docker rm backend-container || true
+                docker run -d --name backend-container -p 8080:8080 $DOCKER_IMAGE
+                '''
+            }
+        }
     }
+
     post {
+        success {
+            echo 'Pipeline executed successfully!'
+        }
+        failure {
+            echo 'Pipeline execution failed!'
+        }
         always {
-           // cleanWs()
+            echo 'Performing cleanup...'
+            cleanWs()
         }
     }
 }
